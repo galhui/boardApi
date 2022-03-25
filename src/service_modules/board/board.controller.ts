02f3d8228947847fc2c4dde223bf9e6342b90a98
An Hyeon Jong <hosi.galhui@gmail.com>
2022-03-26 03:17:46 +0900

    [스웨거 문서 보정]

--- src/service_modules/board/board.controller.ts
+++ src/service_modules/board/board.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create_board.dto';
+import { DetailBoardDto } from './dto/detail_board.dto';
 import { FindBoardQuery } from './dto/find_board.query';
+import { FindBoardResponse } from './dto/find_board.response';
 import { UpdateBoardDto } from './dto/update_board.dto';
 
 @ApiTags('board')
@@ -10,26 +12,35 @@ import { UpdateBoardDto } from './dto/update_board.dto';
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
+  @ApiOperation({ summary: '게시글 작성 API'})
+  @ApiOkResponse({ type: DetailBoardDto })
   @Post()
   create(@Body() createBoardDto: CreateBoardDto) {
     return this.boardService.create(createBoardDto);
   }
 
+  @ApiOperation({ summary: '게시글 목록 API'})
+  @ApiOkResponse({ type: FindBoardResponse})
   @Get()
   findAll(@Query() query: FindBoardQuery) {
     return this.boardService.findAll(query);
   }
 
+  @ApiOperation({ summary: '게시글 상세 API '})
+  @ApiOkResponse({ type: DetailBoardDto })
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.boardService.findOne(id);
   }
 
+  @ApiOperation({ summary: '게시글 수정 API'})
+  @ApiOkResponse({ type: DetailBoardDto })
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body() updateBoardDto: UpdateBoardDto) {
     return this.boardService.update(id, updateBoardDto);
   }
 
+  @ApiOperation({ summary: '게시글 삭제 API'})
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.boardService.remove(id);
